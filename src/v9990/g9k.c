76fd0df6d89cbb0578cfc2d721f53f405cb93886
Pedro de Medeiros <pedro.medeiros@gmail.com>
2023-03-30 23:20:36 -0300

    v9990 functions

---
+++ src/v9990/g9k.c
@@ -0,0 +1,148 @@
+#include "put_pattern.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+// port numbers
+static __sfr __at(G9K_RAM_DATA) port_p0;
+static __sfr __at(G9K_CMD_DATA) port_p2;
+static __sfr __at(G9K_REG_DATA) port_p3;
+static __sfr __at(G9K_REG_SEL)  port_p4;
+static __sfr __at(G9K_SYS_CTRL) port_p7;
+
+
+void set_p1_mode(bool ntsc60hz)
+{
+    port_p7 = 0;
+    set_register(6, 5);
+    set_next_register(ntsc60hz ? 0 : 0b1000); // write to R#7
+    set_register(13, 0);
+
+    // 512kb VRAM
+    set_register(8, 0b00000010);
+}
+
+
+// use Video9000's superimpose control support
+void set_ctrl_port(uint8_t byte) __z88dk_fastcall
+{
+    byte;
+    __asm
+        ld a, l
+        out (#0x6f), a
+    __endasm;
+}
+
+
+void copy_ram_to_vram(void* address, const uint16_t size, uint32_t vram)
+{
+    set_register(0, vram & 0xff);                   // R#0
+    set_next_register((vram >>  8) & 0xff);         // R#1
+    set_next_register((vram >> 16) & 0b111);        // R#2, autoincrement
+
+    for (uint16_t pos = 0; pos < size; ++pos) {
+        port_p0 = ((uint8_t*)address)[pos];
+    } 
+}
+
+
+/* you need to check if the register is readable */
+void enable_register(uint8_t reg, uint8_t bits)
+{
+    port_p4 = reg;                // select register reg
+    // you need to know first if the register is readable
+    uint8_t tmp = port_p3 | bits; // read value and change bits
+    port_p3 = tmp;                // ... and write it back
+}
+
+
+// use disable_registers instead of this function directly
+void _disable_register(uint8_t reg, uint8_t bits)
+{
+    port_p4 = reg;                // select register reg
+    // you need to know first if the register is readable
+    uint8_t tmp = port_p3 & bits; // read value and change bits
+    port_p3 = tmp;                // ... and write it back
+}
+
+
+void set_register(uint8_t reg, uint8_t bits)
+{
+    port_p4 = reg;  // select register reg
+    port_p3 = bits; // ... and write data to P#3
+}
+
+
+inline void set_next_register(uint8_t bits)
+{
+    port_p3 = bits; // ... and write data to P#3
+}
+
+
+// use set_banks_palettes instead of this function directly
+void _set_banks_palettes(uint8_t palettes)
+{
+    set_register(13, palettes);
+}
+
+
+// use set_banks_priorities instead of this function directly
+void _set_banks_priorities(uint8_t priority)
+{
+    set_register(27, priority);
+}
+
+
+uint8_t vpeek(uint32_t address)
+{
+    set_register(3, address & 0xff);                     // R#3
+    set_next_register((address >> 8) & 0xff);            // R#4
+    set_next_register(0x00 | ((address >> 16) & 0b111)); // R#5, autoincrement
+    return port_p0;
+}
+
+
+// vpeek next address used in vpeek
+uint8_t vpeek_next(void)
+{
+    return port_p0;
+}
+
+
+void vpoke(uint32_t address, uint8_t value)
+{
+    set_register(0, address & 0xff);                     // R#0
+    set_next_register((address >> 8) & 0xff);            // R#1
+    set_next_register(0x00 | ((address >> 16) & 0b111)); // R#2, autoincrement
+    port_p0 = value; // write value
+}
+
+
+// vpoke next address used in vpoke
+void vpoke_next(uint8_t value)
+{
+    port_p0 = value;
+}
+
+
+void _put_pattern_a(uint16_t tile, uint16_t dst) {
+    port_p4 = G9K_WRI_MODE;
+    // dst base address: 0x7C000-0x7DFFF (8191 bytes)
+    port_p3 = dst & 0xff;
+    port_p3 = 0xc0 + (dst >> 8);
+    port_p3 = 0x07; // AII (address increment inhibit): advance to next address
+    port_p0 = tile & 0xff;
+    port_p0 = tile >> 8;
+}
+
+
+void _put_pattern_b(uint16_t tile, uint16_t dst) {
+    port_p4 = G9K_WRI_MODE;
+    // dst base address: 0x7E000-0x7FFFF (8191 bytes)
+    port_p3 = dst & 0xff;
+    port_p3 = 0xe0 + (dst >> 8);
+    port_p3 = 0x07; // AII (address increment inhibit): advance to next address
+    port_p0 = tile & 0xff;
+    port_p0 = tile >> 8;
+}
---
+++ src/v9990/g9k.h
@@ -0,0 +1,95 @@
+#ifndef _G9K_H
+#define _G9K_H
+
+
+#include <stdint.h>
+#include <stdbool.h>
+
+
+// sprite stuff
+#define MAX_SPRITES         125
+#define SPRITE_VRAM_ADDRESS 0x10000
+#define SPRITE_DISABLE_BIT  (1<<4)
+
+// port defines
+#define G9K_RAM_DATA                    0x60 // RW
+#define G9K_PAL_DATA                    0x61 // RW
+#define G9K_CMD_DATA                    0x62 // RW
+#define G9K_REG_DATA                    0x63 // RW
+#define G9K_REG_SEL                     0x64 // W
+#define G9K_STA_DATA                    0x65 // R
+#define G9K_INT_FLAG                    0x66 // RW
+#define G9K_SYS_CTRL                    0x67 // W
+#define G9K_SUP_CTRL                    0x6F // RW
+
+// bit flags
+#define G9K_LAYER_B_BIT                 0x40
+#define G9K_LAYER_A_BIT                 0x80
+
+// GFX9000 registers
+#define G9K_WRI_MODE                    0x0
+#define G9K_RED_MODE                    0x3
+
+// extra GFX9000 stuff
+#define MAX_PAL_LEN                     (64 * 3) // 64 colours * 3 components (RGB)
+
+// v9000 control port presets
+#define SUPERIMPOSE_OFF                 0x00
+#define SUPERIMPOSE_TRANSPARENT         0x10
+#define SUPERIMPOSE_GENLOCK             0x18
+
+
+void set_p1_mode(bool ntsc60hz);
+void set_ctrl_port(uint8_t byte) __z88dk_fastcall;
+void enable_fg_layer(void);
+void disable_fg_layer(void);
+void enable_bg_layer(void);
+void disable__bg_layer(void);
+
+
+// register functions
+void set_register(uint8_t reg, uint8_t bits);
+void set_next_register(uint8_t bits);
+uint8_t get_register(uint8_t reg);
+void enable_register(uint8_t reg, uint8_t bits);
+void _disable_register(uint8_t reg, uint8_t bits);
+void enable_interrupts(void);
+void disable_interrupts(void);
+
+#define disable_register(reg, bits)         _disable_register(reg, (uint8_t) ((~(bits)) & 0xff))
+
+// palette functions
+#define set_banks_palettes(b1, b2)          _set_banks_palettes(((b2) << 2) | (b1))
+void _set_banks_palettes(uint8_t palettes);
+
+// priority functions
+#define set_banks_priorities(prx, pry)      _set_banks_priorities(((pry) << 2) | (prx))
+void _set_banks_priorities(uint8_t priority);
+
+
+//
+// VRAM
+//
+  
+void copy_ram_to_vram(void* address, uint16_t size, uint32_t vram);
+
+uint8_t vpeek(uint32_t address);
+
+uint8_t vpeek_next(void);
+
+void vpoke(uint32_t address, uint8_t value);
+
+void vpoke_next(uint8_t value);
+
+
+//
+// Tiles
+//
+
+#define put_pattern_a(tile, dst) do { _put_pattern_a((tile), (dst) << 1); } while(0)
+void _put_pattern_a(uint16_t tile, uint16_t dst);
+
+#define put_pattern_b(tile, dst) do { _put_pattern_b((tile), (dst) << 1); } while(0)
+void _put_pattern_b(uint16_t tile, uint16_t dst);
+
+#endif // _G9K_H
