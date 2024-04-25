f7a30d5fb6afe5fafb0f5bbe1af76d85dc82572c
Pedro de Medeiros <pedro.medeiros@gmail.com>
2024-04-25 15:20:08 -0300

    clean up debug functions

--- src/debugv2/common.c
+++ src/debugv2/common.c
@@ -7,9 +7,6 @@
 #include <stdint.h>
 
 
-uint16_t _stored_numeric_mode = DEBUG_INT;
-
-
 // send value to debug device (control)
 void _out2e(uint8_t value) __z88dk_fastcall
 {
@@ -34,38 +31,7 @@ void _out2f(uint8_t value) __z88dk_fastcall
 
 void debug_mode(uint8_t mode)
 {
-    _stored_numeric_mode = mode;
-}
-
-
-inline void _debug_str(const char* msg)
-{
-    _out2e(DEBUG_CHAR);
-    for (int i = 0; msg[i] != 0; ++i) {
-        _out2f(msg[i]);
-    }
-}
-
-
-void debug_msg(char* msg)
-{
-    _debug_str(msg);
-    _out2f('\n');
-}
-
-
-void _debug_num(uint16_t value)
-{
-    _out2e(_stored_numeric_mode);
-    _out2f(value & 0xff);               // LSB
-    _out2f((value >> 8) & 0xff);        // MSB
-}
-
-
-void debug(char* msg, int value)
-{
-    _debug_str(msg);
-    _debug_num(value);
+    _out2e(mode);
 }
 
 
@@ -78,13 +44,12 @@ void debug_break()
     __endasm;
 }
 
+
 // send printf like structure to tcl script
 inline void _debug_printf(struct debug_printf_data* data)
 {
     UNUSED(data);
     __asm
-        ld a, #DEBUG_PRINTF
-        out (#0x2e), a
         ld c, #0x2f
         out (c), l
         out (c), h
